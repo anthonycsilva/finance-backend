5f5f69207a98db3b946d864f3113dc883276b922
Anthony Silva <Anthony Silva>
2022-11-20 23:22:24 -0300

    criando banco

--- FinanceApi.Api/Program.cs
+++ FinanceApi.Api/Program.cs
@@ -1,3 +1,5 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,7 +16,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
--- FinanceApi.Infra.Data/DataContext.cs
+++ FinanceApi.Infra.Data/DataContext.cs
@@ -4,15 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using FinanceApi.Business.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace FinanceApi.Infra.Data
 {
     public class DataContext : DbContext
     {
         public DbSet<Usuario> Usuarios { get; set; }
+        public DataContext()
+        {
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=Finance;User Id=postgres;Password=postgres;");
         }
     }
 }
